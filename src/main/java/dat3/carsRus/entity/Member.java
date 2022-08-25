c1135f71a0d6911464cba04336b485a7b13a9c2d
Troels <troe1912@stud.kea.dk>
2022-08-25 12:59:03 +0200

    updated Member and Car

--- src/main/java/dat3/carsRus/entity/Member.java
+++ src/main/java/dat3/carsRus/entity/Member.java
@@ -2,7 +2,10 @@ package dat3.carsRus.entity;
 
 import dat3.security.entity.Role;
 import dat3.security.entity.UserWithRoles;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,19 +13,33 @@ import java.util.List;
 @Entity
 public class Member extends UserWithRoles {
 
+    @Column(name = "Fornavn")
     private String firstName;
 
+    @Column(name = "Fornavn")
     private String lastName;
 
+    @Column(name = "Fornavn")
     private int zipCode;
 
+    @Column(name = "Fornavn")
     private String street;
 
+    @Column(name = "Fornavn")
     private String city;
 
+    @Column(name = "Fornavn")
     private boolean isApproved;
 
+    @Column(name = "Fornavn")
     private int ranking;
+
+    @CreationTimestamp
+    private LocalDateTime timeCreated;
+
+    @UpdateTimestamp
+    private LocalDateTime timeEdited;
+
 
     public Member(String username, String email,
                   String password, boolean enabled,
---
+++ src/main/java/dat3/carsRus/repository/MemberRepository.java
@@ -0,0 +1,6 @@
+package dat3.carsRus.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MemberRepository extends CrudRepository {
+}
